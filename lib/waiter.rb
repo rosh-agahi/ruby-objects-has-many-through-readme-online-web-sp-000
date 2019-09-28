75d4060b3a904b6da2b98c6c10ee3340d8694c09
Roshanak Agahi <rosh.agahi@gmail.com>
2019-09-28 06:15:30 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -23,8 +23,9 @@ class Waiter
   end
   
   def best_tipper
-    max.tip
-  
-  def max_tip
-    meals.tip.max
+    best_tipped_meal = meals.max do |meal_a, meal_b|
+      meal_a.tip <=> meal_b.tip
+    end
+    best_tipped_meal.customer
+  end
 end
